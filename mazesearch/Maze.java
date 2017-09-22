ba86970cc06a8e7bb4a702b1c7fdaffff15824eb
Gareth <goduffy22@gmail.com>
2017-09-22 09:56:06 +0100

    first commit

---
+++ mazesearch/Maze.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mazesearch;
+
+/**
+ *
+ * @author Laura
+ */
+public class Maze {//Note: this is an interesting way of not having to create an object 
+    //to solve the problem. Insert a toString class at the end and define what with be printed and then 
+    //the maze object can be empty but still represent everything in its class!!
+    private final int TRIED = 3;
+    private final int PATH = 7;
+    
+    private int[][] grid = { {1,1,1,0,1,1,0,0,0,1,1,1,1},
+                             {1,0,1,1,1,0,1,1,1,1,0,0,1},
+                             {0,0,0,0,1,0,1,0,1,0,1,0,0},
+                             {1,1,1,0,1,1,1,0,1,0,1,1,1},
+                             {1,0,1,0,0,0,0,1,1,1,0,0,1},
+                             {1,0,1,1,1,1,1,1,0,1,1,1,1},
+                             {1,0,0,0,0,0,0,0,0,0,0,0,0},
+                             {1,1,1,1,1,1,1,1,1,1,1,1,1}};
+    
+    public boolean traverse (int row, int column)
+    {
+        boolean done = false;
+        
+        if (valid (row, column))
+        {
+            grid[row][column] = TRIED;
+            
+            if (row == grid.length-1 && column == grid[0].length-1)
+                done = true;
+            else
+            {
+                done = traverse (row+1, column);
+                if(!done)
+                    done = traverse (row, column+1);
+                if(!done)
+                    done = traverse (row-1, column);
+                if(!done)
+                    done = traverse (row, column-1);
+            }
+            
+            if (done)
+            {grid[row][column] = PATH;
+                System.out.print(toString(row, column));}
+        }
+        
+        return done;
+        
+    }
+    
+    private boolean valid (int row, int column)
+    {
+        boolean result = false;
+        
+        if (row >= 0 && row < grid.length && column >=0 && column < grid[row].length)
+            
+            if (grid[row][column] == 1)
+                result = true;
+        return result;
+        }
+    
+    public String toString()
+    {
+        String result = "\n";
+        
+        for (int row=0; row < grid.length; row++)
+        {
+            for (int column=0; column < grid[row].length; column++)
+                result += grid[row][column] + "";
+            result += "\n";
+        }
+        
+        return result;
+    } 
+    
+    public String toString(int row, int column){
+        
+        return "(" + row + "," + column + ")";
+    }
+}
